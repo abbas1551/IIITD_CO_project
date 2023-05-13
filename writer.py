5cc7e7d7695e5678469d800687ae2f62f40e7bca
ANGADJEET <119041931+ANGADJEET@users.noreply.github.com>
2023-05-13 18:36:17 +0530

    Update writer.py
    extended for entire input.

--- writer.py
+++ writer.py
@@ -5,8 +5,12 @@ def write(filename, list):
     for i in range(len(list)):
         if list[i] == []:
             continue
-        file.write(list[i][0])
+        for j in range(len(list[i])):
+
+            file.write(list[i][j])
+            #file.write("_")
         file.write("\n")
+    file.close()
 
 output_file_name = "output.txt"
 output_list = machinecode
