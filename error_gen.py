09200166b4293cb967d6772ca0b6b80a92d63001
ANGADJEET <119041931+ANGADJEET@users.noreply.github.com>
2023-05-15 19:49:07 +0530

    Update error_gen.py
    minor spell checks

--- error_gen.py
+++ error_gen.py
@@ -42,13 +42,13 @@ for i in final_data:
         if len(i) != 4:
             print(i[0], "takes 3 arguments", len(i)-1, "given")
             error_present = True
-    elif ((i[0] == 'ls') or (i[0] == 'rs')):
-        if len(i[0]) != 3:
-            print(i[0], "takes 2 arguments", len(i)-1, "given")
-            error_present = True
-        elif (i[2][0] != '$'):
-            print("must be a $")
-            error_present = True
+    # elif ((i[0] == 'ls') or (i[0] == 'rs')):
+    #     if len(i[0]) != 3:
+    #         print(i[0], "takes 2 arguments", len(i)-1, "given")
+    #         error_present = True
+    #     elif (i[2][0] != '$'):
+    #         print("must be a $")
+    #         error_present = True
     elif ((i[0] not in ISA) and (i[0] != 'var')):  #condition for unknown command
         if (':' not in i[0]):
             print("Error", i[0], "not a recognised command")
@@ -58,7 +58,7 @@ for i in final_data:
           
 
 if hlt_count == 0:
-    print('No halt statement cound')
+    print('No halt statement found')
     error_present = True
 
 if final_data[-1][-1] != 'hlt':  #checks for last command to be halt
@@ -68,5 +68,5 @@ if final_data[-1][-1] != 'hlt':  #checks for last command to be halt
 for i in final_data:  #checks for valid memeory address
     if ((i[0] == 'jmp') or (i[0] == 'jlt') or (i[0] == 'jgt') or (i[0] == 'je')):  
         if i[1]+':' not in lables:
-            print(i[1],"lable not found")
+            print(i[1],"label not found")
             error_present = True
