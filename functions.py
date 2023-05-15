613e06d4297e7ecf7ba4207f3940acc8e3029114
arnav22098 <122359510+arnav22098@users.noreply.github.com>
2023-05-15 20:48:33 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,8 +3,10 @@ import reader
 def dec_to_bin(dec_str):
     dec_str = int(dec_str[1:])
     binary = bin(dec_str)[2:]
+    
     binary = binary.zfill(7)
     return binary
+
 
 def gen_address(count):
     address = bin(count)[2:]
