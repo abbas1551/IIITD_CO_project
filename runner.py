2521606016ceff334960414dd88be0f92e4a741d
abbas1551 <119072166+abbas1551@users.noreply.github.com>
2023-05-15 14:27:50 +0530

    Create runner.py
    this file lets us run the entire code in one go

---
+++ runner.py
@@ -0,0 +1,27 @@
+import subprocess
+from error_gen import error_present
+
+file_error_gen = 'error_gen.py'
+running_error_gen = subprocess.Popen(['python', file_error_gen], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+if (error_present == False):
+    file1_to_run = 'writer.py'
+    file2_to_run = 'translator.py'
+
+
+    process1 = subprocess.Popen(['python', file1_to_run], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process2 = subprocess.Popen(['python', file2_to_run], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout, stderr = process1.communicate()
+
+
+
+    stdout = stdout.decode('utf-8')
+    stderr = stderr.decode('utf-8')
+
+    # Print the output
+    print('Standard Output:')
+    print(stdout)
+    print('Standard Error:')
+    print(stderr)
