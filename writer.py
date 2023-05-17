b2af0df14905f734c7d711b3910947a2920620d0
ANGADJEET <119041931+ANGADJEET@users.noreply.github.com>
2023-05-17 15:14:03 +0530

    Update writer.py

--- writer.py
+++ writer.py
@@ -1,21 +1,32 @@
-from translator import machinecode
+import error_gen
 
-def write(filename, list):
-    file = open(filename, 'w')
-    for i in range(len(list)):
-        if list[i] == []:
-            continue
-        for j in range(len(list[i])):
+con = error_gen.error_present
 
-            file.write(list[i][j])
-            #file.write("_")
-        file.write("\n")
-    file.close()
+if con == False:
+    from translator import machinecode
 
-output_file_name = "output.txt"
-output_list = machinecode
+    def write(filename, list):
+        file = open(filename, 'w')
+        for i in range(len(list)):
+            if list[i] == []:
+                continue
+            for j in range(len(list[i])):
+
+                file.write(list[i][j])
+                #file.write("_")
+            file.write("\n")
+        file.close()
+
+    output_file_name = "output.txt"
+    output_list = machinecode
 
 
 
 
-write(output_file_name, output_list)
+    write(output_file_name, output_list)
+
+
+    out = open("output.txt", 'r')
+    out = out.read()
+    import sys
+    sys.stdout.write(out)
